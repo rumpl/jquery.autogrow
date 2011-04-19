ba97e583abb30ecfea69c209705275956b9ed0f6
Djordje Lukic <lukic.djordje@gmail.com>
2011-04-19 11:35:13 +0200

    Initial commit of the autogrow plugin

---
+++ jquery.autogrow.js
@@ -0,0 +1,40 @@
+(function ($) {
+    $.fn.autogrow = function () {
+        this.filter('textarea').each(function () {
+            var $this = $(this),
+                minHeight = $this.height(),
+                shadow = $('<div></div>').css({
+                    position:   'absolute',
+                    top: -10000,
+                    left: -10000,
+                    width: $(this).width(),
+                    fontSize: $this.css('fontSize'),
+                    fontFamily: $this.css('fontFamily'),
+                    lineHeight: $this.css('lineHeight'),
+                    resize: 'none'
+                }).addClass('shadow').appendTo(document.body),
+                update = function () {
+                    var t = this;
+                    setTimeout(function () {
+                        var val = t.value.replace(/</g, '&lt;')
+                                .replace(/>/g, '&gt;')
+                                .replace(/&/g, '&amp;')
+                                .replace(/\n/g, '<br/>&nbsp;');
+    
+                        if (val === '' || $.trim(val) === '') {
+                            val = 'a';
+                        }
+    
+                        shadow.html(val);
+                        $(t).css('height', Math.max(shadow[0].offsetHeight + 20, minHeight));
+                    }, 0);
+                };
+
+            $(this).change(update).keyup(update).keydown(update).focus(update);
+            update.apply(this);
+        });
+
+        return this;
+    };
+
+}(jQuery));
